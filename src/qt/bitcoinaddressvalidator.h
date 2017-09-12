3cf1548a35231acb0bf3e1b35db4af4d48b52323
oneyesoneno <oneyesoneno@gmail.com>
2017-09-12 17:47:05 +0700

    Rilis Pertama

---
+++ src/qt/bitcoinaddressvalidator.h
@@ -0,0 +1,25 @@
+#ifndef BITCOINADDRESSVALIDAOYON_H
+#define BITCOINADDRESSVALIDAOYON_H
+
+#include <QRegExpValidator>
+
+/** Base48 entry widget validator.
+   Corrects near-miss characters and refuses characters that are no part of base48.
+ */
+class BitcoinAddressValidator : public QValidator
+{
+    Q_OBJECT
+public:
+    explicit BitcoinAddressValidator(QObject *parent = 0);
+
+    State validate(QString &input, int &pos) const;
+
+    //static const int MaxAddressLength = 35;
+    static const int MaxAddressLength = 128; // accept stealth addresses
+signals:
+
+public slots:
+
+};
+
+#endif // BITCOINADDRESSVALIDAOYON_H
